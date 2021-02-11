0e0881bf726b7ae49e3ba0a107ece35550975e24
Stefan Froemken <froemken@gmail.com>
2021-02-11 21:51:35 +0100

    Replace  in ext_table and ext_localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,8 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
-    $extConf = is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]) ?: unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['mysqlreport'])) {
+    $extConf = is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['mysqlreport']) ?: unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['mysqlreport']);
     if (
         ($extConf['profileFrontend'] && TYPO3_MODE === 'FE') ||
         ($extConf['profileBackend'] && TYPO3_MODE === 'BE')
@@ -19,10 +19,8 @@ if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
     }
 }
 
-if (TYPO3_MODE === 'BE') {
-    // add button to clear cache of profiling table
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions'][]
-        = 'StefanFroemken\\Mysqlreport\\Backend\\CacheAction';
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][]
-        = 'StefanFroemken\\Mysqlreport\\Backend\\CacheAction->clearProfiles';
-}
+// add button to clear cache of profiling table
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions'][]
+    = 'StefanFroemken\\Mysqlreport\\Backend\\CacheAction';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][]
+    = 'StefanFroemken\\Mysqlreport\\Backend\\CacheAction->clearProfiles';
--- ext_tables.php
+++ ext_tables.php
@@ -3,21 +3,19 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-if (TYPO3_MODE === 'BE') {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'StefanFroemken.' . $_EXTKEY,
-        'system', // Make module a submodule of 'web'
-        'mysql', // Submodule key
-        '', // Position
-        array(
-            'MySql' => 'index, queryCache, innoDbBuffer, threadCache, tableCache, report',
-            'Profile' => 'list, show, queryType, profileInfo',
-            'Query' => 'filesort, fullTableScan',
-        ),
-        array(
-            'access' => 'user,group',
-            'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.svg',
-            'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_report.xlf',
-        )
-    );
-}
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'StefanFroemken.Mysqlreport',
+    'system', // Make module a submodule of 'web'
+    'mysql', // Submodule key
+    '', // Position
+    array(
+        'MySql' => 'index, queryCache, innoDbBuffer, threadCache, tableCache, report',
+        'Profile' => 'list, show, queryType, profileInfo',
+        'Query' => 'filesort, fullTableScan',
+    ),
+    array(
+        'access' => 'user,group',
+        'icon'   => 'EXT:mysqlreport/ext_icon.svg',
+        'labels' => 'LLL:EXT:mysqlreport/Resources/Private/Language/locallang_report.xlf',
+    )
+);
