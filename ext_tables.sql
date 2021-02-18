4772a028324e88b32d0e9a5514f002d47e026b8d
Stefan Froemken <froemken@gmail.com>
2021-02-18 21:23:40 +0100

    Remove TYPO3 columns

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,9 +2,6 @@
 # Table structure for table 'tx_mysqlreport_domain_model_profile'
 #
 CREATE TABLE tx_mysqlreport_domain_model_profile (
-    uid int(11) NOT NULL auto_increment,
-    pid int(11) DEFAULT '0' NOT NULL,
-
     query_id int(11) unsigned DEFAULT '0',
     mode char(3) DEFAULT '' NOT NULL,
     ip varchar(45) DEFAULT '' NOT NULL,
@@ -19,8 +16,5 @@ CREATE TABLE tx_mysqlreport_domain_model_profile (
     not_using_index tinyint(1) DEFAULT '0' NOT NULL,
     using_fulltable tinyint(1) DEFAULT '0' NOT NULL,
 
-    crdate int(11) unsigned DEFAULT '0' NOT NULL,
-
-    PRIMARY KEY (uid),
     KEY profileCalls (unique_call_identifier,crdate,mode,duration)
 );
