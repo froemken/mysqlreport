e50131fed69904b7c7b139b3e82de2a9b3d7a179
Stefan Froemken <froemken@gmail.com>
2022-11-20 22:01:02 +0100

    Add event to modify profile records. Round duration to precision 6

---
+++ Classes/Event/ModifyProfileRecordsEvent.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package stefanfroemken/mysqlreport.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace StefanFroemken\Mysqlreport\Event;
+
+/**
+ * Event to modify the profile records in ProfileRepository
+ * There is no setProfileRecords in this class, as I don't want that someone removes all records.
+ */
+class ModifyProfileRecordsEvent
+{
+    /**
+     * @var string
+     */
+    private $methodName;
+
+    /**
+     * @var array
+     */
+    private $profileRecords;
+
+    public function __construct(string $methodName, array $profileRecords)
+    {
+        $this->methodName = $methodName;
+        $this->profileRecords = $profileRecords;
+    }
+
+    public function getMethodName(): string
+    {
+        return $this->methodName;
+    }
+
+    public function getProfileRecords(): array
+    {
+        return $this->profileRecords;
+    }
+
+    public function updateProfileRecord(int $key, array $profileRecord): void
+    {
+        $this->profileRecords[$key] = $profileRecord;
+    }
+}
