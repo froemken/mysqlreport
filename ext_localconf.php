a0595d65472b3d6e6517607bf2b196a99b692027
Stefan Froemken <froemken@gmail.com>
2022-11-23 20:25:31 +0100

    Migrate CacheAction to EventListener

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,11 +6,3 @@ if (!defined('TYPO3_MODE')) {
 // Add Debug Logger to Doctrine via first Hook in TYPO3
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['extTablesInclusion-PostProcessing'][]
     = \StefanFroemken\Mysqlreport\Hook\RegisterDatabaseLoggerHook::class;
-
-// add button to clear cache of profiling table
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions'][]
-    = \StefanFroemken\Mysqlreport\Backend\CacheAction::class;
-
-// process truncate of MySQL profiles
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][]
-    = \StefanFroemken\Mysqlreport\Backend\CacheAction::class . '->clearProfiles';
