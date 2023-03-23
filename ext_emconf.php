e3079aaeef8ad958e4a4a3bd45930603ac660d62
Stefan Froemken <froemken@gmail.com>
2023-03-23 23:09:37 +0100

    Update version to 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'MySQL Report',
-    'description' => 'Analyze and profile your TYPO3 databases queries',
+    'description' => 'Analyze and profile your TYPO3 database queries',
     'category' => 'module',
     'author' => 'Stefan Froemken',
     'author_email' => 'froemken@gmail.com',
     'state' => 'stable',
     'author_company' => '',
-    'version' => '2.1.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.23-12.5.99',
