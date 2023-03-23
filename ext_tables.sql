f2e70d8ff59397ecb1cd545870812d3ff378e605
Stefan Froemken <froemken@gmail.com>
2023-03-23 22:54:45 +0100

    Reformat ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,20 +1,21 @@
 #
 # Table structure for table 'tx_mysqlreport_domain_model_profile'
 #
-CREATE TABLE tx_mysqlreport_domain_model_profile (
-    query_id int(11) unsigned DEFAULT '0',
-    mode char(3) DEFAULT '' NOT NULL,
-    ip varchar(45) DEFAULT '' NOT NULL,
-    request text,
-    referer text,
-    unique_call_identifier varchar(24) DEFAULT '' NOT NULL,
-    duration double(11,8) DEFAULT '0.00000000' NOT NULL,
-    query blob NOT NULL,
-    query_type varchar(20) DEFAULT '' NOT NULL,
-    profile text NOT NULL,
-    explain_query text NOT NULL,
-    not_using_index tinyint(1) DEFAULT '0' NOT NULL,
-    using_fulltable tinyint(1) DEFAULT '0' NOT NULL,
+CREATE TABLE tx_mysqlreport_domain_model_profile
+(
+	query_id               int(11) unsigned DEFAULT '0',
+	mode                   char(3)      DEFAULT ''           NOT NULL,
+	ip                     varchar(45)  DEFAULT ''           NOT NULL,
+	request                text,
+	referer                text,
+	unique_call_identifier varchar(24)  DEFAULT ''           NOT NULL,
+	duration               double(11,8) DEFAULT '0.00000000' NOT NULL,
+	query                  blob                              NOT NULL,
+	query_type             varchar(20)  DEFAULT ''           NOT NULL,
+	profile                text                              NOT NULL,
+	explain_query          text                              NOT NULL,
+	not_using_index        tinyint(1)   DEFAULT '0'          NOT NULL,
+	using_fulltable        tinyint(1)   DEFAULT '0'          NOT NULL,
 
-    KEY profileCalls (unique_call_identifier,crdate,mode,duration)
+	KEY                    profileCalls (unique_call_identifier,crdate,mode,duration)
 );
