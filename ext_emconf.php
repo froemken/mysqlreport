4a6787f1522af68dfa3135802a93d26b40eef2f7
Stefan Froemken <froemken@gmail.com>
2023-11-15 20:55:06 +0100

    Update dependencies to secure TYPO3 versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.23-12.5.99',
+            'typo3' => '11.5.33-12.4.99',
         ],
         'conflicts' => [
         ],
