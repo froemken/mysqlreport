3c943fb03b4bb9250477c2f8fae76e0788d385af
Stefan Froemken <froemken@gmail.com>
2024-08-08 23:53:55 +0200

    Add interface for our mysql logger

---
+++ Classes/Logger/LoggerInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package stefanfroemken/mysqlreport.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace StefanFroemken\Mysqlreport\Logger;
+
+use Doctrine\DBAL\ParameterType;
+use StefanFroemken\Mysqlreport\Domain\Model\QueryInformation;
+
+interface LoggerInterface
+{
+    /**
+     * This method will be called just after the query has been executed by doctrine.
+     * Start collecting duration and other stuff.
+     *
+     * @param array<int, string> $params
+     * @param array<int, ParameterType> $types
+     */
+    public function stopQuery(string $query, float $duration, array $params = [], array $types = []): ?QueryInformation;
+}
