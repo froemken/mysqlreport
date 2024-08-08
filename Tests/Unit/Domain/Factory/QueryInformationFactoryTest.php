2d9b8bc0b455c6661cb1a0c4edfe3b1685db4a25
Stefan Frömken <froemken@gmail.com>
2024-08-09 01:37:20 +0200

    Add test for QueryInformationFactory (#112)
    * Add test for QueryInformationFactory
    
    * Update lines mentioned by php-cs-fixer

--- Tests/Unit/Domain/Factory/QueryInformationFactoryTest.php
+++ Tests/Unit/Domain/Factory/QueryInformationFactoryTest.php
@@ -13,6 +13,7 @@ namespace StefanFroemken\Mysqlreport\Tests\Unit\Domain\Factory;
 
 use PHPUnit\Framework\Attributes\Test;
 use StefanFroemken\Mysqlreport\Domain\Factory\QueryInformationFactory;
+use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
 use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Routing\PageArguments;
@@ -25,11 +26,37 @@ class QueryInformationFactoryTest extends UnitTestCase
 {
     private QueryInformationFactory $subject;
 
+    private int $time;
+
     protected function setUp(): void
     {
-        $GLOBALS['TYPO3_REQUEST'] = (new ServerRequest('https://www.example.com/'))
+        $this->time = time();
+
+        $GLOBALS['EXEC_TIME'] = $this->time;
+
+        $_SERVER = array_merge($_SERVER, [
+            'REMOTE_ADDR' => '123.124.125.126',
+            'HTTP_HOST' => 'example.com',
+            'HTTP_REFERER' => 'https://www.typo3lexikon.de',
+            'REQUEST_URI' => '/index.php',
+            'SSL_SESSION_ID' => '1234567890',
+        ]);
+
+        $GLOBALS['TYPO3_REQUEST'] = (new ServerRequest('https://www.example.com/', 'GET', 'php://input', [], $_SERVER))
             ->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_FE)
             ->withAttribute('routing', new PageArguments(1, '0', []));
+
+        Environment::initialize(
+            Environment::getContext(),
+            false,
+            Environment::isComposerMode(),
+            Environment::getProjectPath(),
+            Environment::getPublicPath(),
+            Environment::getVarPath(),
+            Environment::getConfigPath(),
+            Environment::getCurrentScript(),
+            Environment::isWindows() ? 'WINDOWS' : 'UNIX',
+        );
 
         $this->subject = new QueryInformationFactory();
     }
@@ -38,15 +65,87 @@ class QueryInformationFactoryTest extends UnitTestCase
     {
         unset(
             $this->subject,
+            $GLOBALS['TYPO3_REQUEST'],
+            $GLOBALS['EXEC_TIME'],
         );
     }
 
     #[Test]
-    public function createNewQueryInformationWillCreateQueryInformationWithPid1(): void
+    public function createNewQueryInformationWillCreateQueryInformationWithFrontendPid(): void
     {
         self::assertSame(
             1,
             $this->subject->createNewQueryInformation()->getPid(),
+        );
+    }
+
+    #[Test]
+    public function createNewQueryInformationWillCreateQueryInformationWithBackendPid(): void
+    {
+        $GLOBALS['TYPO3_REQUEST'] = (new ServerRequest('https://www.example.com/typo3/?id=30', 'GET', 'php://input', [], $_SERVER))
+            ->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_BE)
+            ->withQueryParams(['id' => 30]);
+
+        $this->subject = new QueryInformationFactory();
+
+        self::assertSame(
+            30,
+            $this->subject->createNewQueryInformation()->getPid(),
+        );
+    }
+
+    #[Test]
+    public function createNewQueryInformationWillCreateQueryInformationWithIp(): void
+    {
+        self::assertSame(
+            '123.124.125.126',
+            $this->subject->createNewQueryInformation()->getIp(),
+        );
+    }
+
+    #[Test]
+    public function createNewQueryInformationWillCreateQueryInformationWithReferer(): void
+    {
+        self::assertSame(
+            'https://www.typo3lexikon.de',
+            $this->subject->createNewQueryInformation()->getReferer(),
+        );
+    }
+
+    #[Test]
+    public function createNewQueryInformationWillCreateQueryInformationWithRequest(): void
+    {
+        self::assertSame(
+            'https://example.com/index.php',
+            $this->subject->createNewQueryInformation()->getRequest(),
+        );
+    }
+
+    #[Test]
+    public function createNewQueryInformationWillCreateQueryInformationWithTypo3Mode(): void
+    {
+        self::assertSame(
+            'FE',
+            $this->subject->createNewQueryInformation()->getMode(),
+        );
+    }
+
+    #[Test]
+    public function createNewQueryInformationWillCreateQueryInformationWithCallIdentifier(): void
+    {
+        // We are working with more entropy
+        self::assertStringContainsString(
+            '.',
+            $this->subject->createNewQueryInformation()->getUniqueCallIdentifier(),
+        );
+    }
+
+    #[Test]
+    public function createNewQueryInformationWillCreateQueryInformationWithCrdate(): void
+    {
+        self::assertSame(
+            $this->time,
+            $this->subject->createNewQueryInformation()->getCrdate(),
         );
     }
 }
