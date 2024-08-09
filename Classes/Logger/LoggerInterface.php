691bfb575056ba64f011953de25cc4a20f7b38d7
Stefan Froemken <froemken@gmail.com>
2024-08-09 03:28:02 +0200

    Remove unused use statements

--- Classes/Logger/LoggerInterface.php
+++ Classes/Logger/LoggerInterface.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace StefanFroemken\Mysqlreport\Logger;
 
-use Doctrine\DBAL\ParameterType;
 use StefanFroemken\Mysqlreport\Domain\Model\QueryInformation;
 
 interface LoggerInterface
